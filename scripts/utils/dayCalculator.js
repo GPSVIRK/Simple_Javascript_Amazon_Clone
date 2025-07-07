f1e9e456d53d15345ff2959a230c318f92d69232
GPSVIRK <gpsvirk1@gmail.com>
2025-07-07 21:59:40 +0530

    i dont know why its like this but it is, weekend calculations are now correct and stupid

--- scripts/utils/dayCalculator.js
+++ scripts/utils/dayCalculator.js
@@ -1,17 +1,21 @@
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'; //we used a default export, its for when you want to export only one thing, there is only one default export allowed, the normal version is called named export
 
-export function dayCalculator(deliveryOption){
-	const today = dayjs().subtract(2,'days');
-	
-	let deliveryDate = today.add(
-		deliveryOption.deliveryDays,
-		'days'
-	);
+function isWeekend(dateObj){
+	return dateObj.format('dddd') === 'Saturday' || dateObj.format('dddd') === 'Sunday';
+}
 
-	if(deliveryDate.format('dddd') === 'Saturday'){
-		deliveryDate = deliveryDate.add(2,'days');
-	} else if (deliveryDate.format('dddd') === 'Sunday'){
+export function dayCalculator(deliveryOption){
+	const today = dayjs();
+
+	let daysToAdd = deliveryOption.deliveryDays;
+
+	let deliveryDate = today;
+
+	while(daysToAdd > 0){
 		deliveryDate = deliveryDate.add(1,'days');
+		if (!isWeekend(deliveryDate)){
+			daysToAdd--;
+		}
 	}
 
 	const dateString = deliveryDate.format('dddd, MMMM D');
