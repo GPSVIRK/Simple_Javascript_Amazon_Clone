f50fedca902e09df7dcc233677fd6f38f4b880b8
GPSVIRK <gpsvirk1@gmail.com>
2025-07-10 17:37:34 +0530

    use promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,9 +3,19 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckOutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
 
-
-loadProducts(() => {
+new Promise((resolve) => { //callbacks like below cause a lot of nesting, so this is better
+    loadProducts(() => {
+        resolve();
+    });
+}).then(() => {
     renderCheckOutHeader();
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+// loadProducts(() => {
+//     renderCheckOutHeader();
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
