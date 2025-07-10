83646d8449ce4894dc38e43624757d86741088ac
GPSVIRK <gpsvirk1@gmail.com>
2025-07-10 18:05:00 +0530

    convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,18 +4,57 @@ import { renderCheckOutHeader } from "./checkout/checkoutHeader.js";
 import { loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 
-Promise.all([ // this is so that we can do all the promises at once instead of one by one 
-    loadProductsFetch(),// it returns a promise directly
-    new Promise((resolve) => {
-        loadCart(() => {
-            resolve();
-        });
+async function loadPage(){// this is a shorthand way to return a promise, returning inside this function is like keeping a value inside the resolve thing, we use this cuz of the await feature, but we can only use await inside an async function
+  await loadProductsFetch();
+
+	await new Promise((resolve) => {
+		loadCart(() => {
+			resolve();
+		});
+	});
+
+	renderCheckOutHeader();
+	renderOrderSummary();
+	renderPaymentSummary();
+}
+
+loadPage();
+
+/*
+  shorthand for this:
+  function loadPage(){
+    return new Promise((resolve) => {
+			[code]
+			resolve();
     })
+  }
+
+	when using await if we have a resolve that has an argument then we can save it in a value cuz it gets returned
+
+	const value = await new Promise((resolve) => {
+		[code]
+		resolve('hi');
+	});
+
+	then <value> has the value <'hi'> stored in it
+*/
+
+
+//this is too extra
+/*
+Promise.all([ // this is so that we can do all the promises at once instead of one by one 
+	loadProductsFetch(),// it returns a promise directly
+	new Promise((resolve) => {
+		loadCart(() => {
+			resolve();
+		});
+	})
 ]).then(() => {
-    renderCheckOutHeader();
-    renderOrderSummary();
-    renderPaymentSummary();
+	renderCheckOutHeader();
+	renderOrderSummary();
+	renderPaymentSummary();
 });
+*/
 
 
 
